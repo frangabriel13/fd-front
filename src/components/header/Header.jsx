5cb3087aeaad9a46a9929053988a94f74869f1e1
Franco <mansilla.franco.1@gmail.com>
2024-11-23 18:37:04 -0300

    cambios

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,3 @@
-// import s from './Header.module.css';
-// import logo from '../../assets/logo.jpg';
-// import SearchBar from '../searchBar/SearchBar';
-
-// const Header = () => {
-//   return (
-//     <div className={s.container}>
-//       <div className={s.topHeader}>
-//         <img src={logo} alt="Logo" className={s.logo} />
-//         <SearchBar />
-//         <div>Ofertas Exclusivas solo por tiempo limitado</div>
-//       </div>
-//       <div></div>
-//     </div>
-//   )
-// };
-
-
-// export default Header;
-
 import s from './Header.module.css';
 import logo from '../../assets/logo.jpg';
 import SearchBar from '../searchBar/SearchBar';
@@ -31,7 +11,7 @@ const Header = () => {
       <div className={s.search}>
         <SearchBar />
       </div>
-      <div className={s.message}>Ofertas por tiempo limitado</div>
+      <div className={s.message}>Ofertas Exclusivas por tiempo limitado</div>
       <div className={s.placeholder}>Placeholder</div>
       <div className={s.navbar}>Navbar</div>
       <div className={s.login}>Ingresa</div>
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,15 @@
 import s from './SearchBar.module.css';
+import { FaSearch } from 'react-icons/fa';
 
 const SearchBar = () => {
   return (
     <div className={s.container}>
-      <input type="text" className={s.input} placeholder="Search" />
+      <div className={s.divSearch}>
+        <input type="text" className={s.input} placeholder="Buscar productos, fabricantes y más..." />
+        <button className={s.searchBtn}>
+          <FaSearch className={s.icon} />
+        </button>
+      </div>
     </div>
   );
 };

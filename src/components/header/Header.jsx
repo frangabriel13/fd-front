6d41b3dab3e4cc13ddbacf8e21ad24244f4616b5
Franco <mansilla.franco.1@gmail.com>
2024-11-23 16:15:57 -0300

    cambios

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,23 @@
+// import s from './Header.module.css';
+// import logo from '../../assets/logo.jpg';
+// import SearchBar from '../searchBar/SearchBar';
+
+// const Header = () => {
+//   return (
+//     <div className={s.container}>
+//       <div className={s.topHeader}>
+//         <img src={logo} alt="Logo" className={s.logo} />
+//         <SearchBar />
+//         <div>Ofertas Exclusivas solo por tiempo limitado</div>
+//       </div>
+//       <div></div>
+//     </div>
+//   )
+// };
+
+
+// export default Header;
+
 import s from './Header.module.css';
 import logo from '../../assets/logo.jpg';
 import SearchBar from '../searchBar/SearchBar';
@@ -5,12 +25,18 @@ import SearchBar from '../searchBar/SearchBar';
 const Header = () => {
   return (
     <div className={s.container}>
-      <div className={s.topHeader}>
+      <div className={s.divLogo}>
         <img src={logo} alt="Logo" className={s.logo} />
+      </div>
+      <div className={s.search}>
         <SearchBar />
       </div>
+      <div className={s.message}>Ofertas por tiempo limitado</div>
+      <div className={s.placeholder}>Placeholder</div>
+      <div className={s.navbar}>Navbar</div>
+      <div className={s.login}>Ingresa</div>
     </div>
-  )
+  );
 };
 
 

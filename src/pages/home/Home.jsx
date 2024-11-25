56d9c1c9fe378cb097eb5cc0e7a176ccfeaceff5
Franco <mansilla.franco.1@gmail.com>
2024-11-25 17:12:50 -0300

    cambios0

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import Banner from '../../components/banner/Banner';
+import Features from '../../components/features/Features';
 import s from './Home.module.css';
 
 const Home = () => {
   return (
-    <div className={s.divContainer}>
+    <div className={s.container}>
       <Banner />
+      <div className={s.home}>
+        <Features />
+      </div>
     </div>
   );
 };

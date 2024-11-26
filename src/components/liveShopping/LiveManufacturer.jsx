cfa31615854025b9c8a9915884d40073b77bab26
Franco <mansilla.franco.1@gmail.com>
2024-11-26 16:05:14 -0300

    cambios

--- src/components/liveShopping/LiveManufacturer.jsx
+++ src/components/liveShopping/LiveManufacturer.jsx
@@ -1,9 +1,13 @@
 import s from './LiveManufacturer.module.css';
 
 const LiveManufacturer = ({ manufacturer }) => {
+
   return (
     <div className={s.container}>
-      <img className={s.image} src={manufacturer.image} alt={manufacturer.name} />
+      <div className={s.divImage}>
+        <img src={manufacturer.image} alt={manufacturer.name} className={manufacturer.live ? `${s.image} ${s.liveImage}` : s.image} />
+        { manufacturer.live && <div className={s.live}>LIVE</div> }
+      </div>
       <button className={s.btnMore}>Ver tienda</button>
     </div>
   )

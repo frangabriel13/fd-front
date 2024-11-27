5487e1f96b288b1e2edaac92cde9a4778b882ff8
Franco <mansilla.franco.1@gmail.com>
2024-11-27 14:29:31 -0300

    cambios

--- src/components/liveShopping/LiveShopping.jsx
+++ src/components/liveShopping/LiveShopping.jsx
@@ -8,22 +8,10 @@ import { GrNext, GrPrevious } from "react-icons/gr";
 const LiveShopping = () => {
   const dispatch = useDispatch();
   const liveManufacturers = useSelector((state) => state.manufacturer.liveManufacturers);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const itemsPerPage = 8;
 
   useEffect(() => {
     dispatch(getLiveManufacturers());
   }, [dispatch]);
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => Math.max(prevIndex - itemsPerPage, 0));
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => Math.min(prevIndex + itemsPerPage, liveManufacturers.length - itemsPerPage));
-  };
-
-  const visibleManufacturers = liveManufacturers.slice(currentIndex, currentIndex + itemsPerPage);
 
   return (
     <div className={s.container}>
@@ -31,14 +19,16 @@ const LiveShopping = () => {
         <h2 className={s.title}>Live Shopping</h2>
         <button className={s.btnMore}>Ver más</button>
       </div>
-      <div className={s.liveManufacturers}>
-        <button className={s.btnPrev} onClick={handlePrev} disabled={currentIndex === 0}>
+      <div className={s.navigation}>
+        <button className={s.navButton}>
           <GrPrevious />
         </button>
-        {visibleManufacturers.map((manufacturer, index) => (
-          <LiveManufacturer key={`${manufacturer.id}-${index}`} manufacturer={manufacturer} />
-        ))}
-        <button className={s.btnNext} onClick={handleNext} disabled={currentIndex + itemsPerPage >= visibleManufacturers.length}>
+        <div className={s.liveManufacturers}>
+          {liveManufacturers.map((manufacturer, index) => (
+            <LiveManufacturer key={`${manufacturer.id}-${index}`} manufacturer={manufacturer} />
+          ))}
+        </div>
+        <button className={s.navButton}>
           <GrNext />
         </button>
       </div>
@@ -47,54 +37,3 @@ const LiveShopping = () => {
 };
 
 export default LiveShopping;
-// import { useEffect, useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import s from './LiveShopping.module.css';
-// import { getLiveManufacturers } from '../../store/actions/manufacturerActions';
-// import LiveManufacturer from './LiveManufacturer';
-// import { GrNext, GrPrevious } from "react-icons/gr";
-
-// const LiveShopping = () => {
-//   const dispatch = useDispatch();
-//   const liveManufacturers = useSelector((state) => state.manufacturer.liveManufacturers);
-//   const [currentIndex, setCurrentIndex] = useState(0);
-//   const itemsPerPage = 8;
-
-//   useEffect(() => {
-//     dispatch(getLiveManufacturers());
-//   }, [dispatch]);
-
-//   const handlePrev = () => {
-//     setCurrentIndex((prevIndex) => Math.max(prevIndex - itemsPerPage, 0));
-//   };
-
-//   const handleNext = () => {
-//     setCurrentIndex((prevIndex) => Math.min(prevIndex + itemsPerPage, liveManufacturers.length * 8 - itemsPerPage));
-//   };
-
-//   // Multiplicar el array de fabricantes por 8
-//   const multipliedManufacturers = Array(8).fill(liveManufacturers).flat();
-//   const visibleManufacturers = multipliedManufacturers.slice(currentIndex, currentIndex + itemsPerPage);
-
-//   return (
-//     <div className={s.container}>
-//       <div className={s.divTitle}>
-//         <h2 className={s.title}>Live Shopping</h2>
-//         <button className={s.btnMore}>Ver más</button>
-//       </div>
-//       <div className={s.liveManufacturers}>
-//         <button className={s.btnPrev} onClick={handlePrev} disabled={currentIndex === 0}>
-//           <GrPrevious />
-//         </button>
-//         {visibleManufacturers.map((manufacturer, index) => (
-//           <LiveManufacturer key={`${manufacturer.id}-${index}`} manufacturer={manufacturer} />
-//         ))}
-//         <button className={s.btnNext} onClick={handleNext} disabled={currentIndex + itemsPerPage >= multipliedManufacturers.length}>
-//           <GrNext />
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LiveShopping;

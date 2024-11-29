d93a5a3d334e15240e0e85abdcf7a0d9a695f7de
Franco <mansilla.franco.1@gmail.com>
2024-11-29 13:22:29 -0300

    cambios

---
+++ src/components/productCard/ProductCard.jsx
@@ -0,0 +1,21 @@
+import s from './ProductCard.module.css';
+
+const ProductCard = ({ product }) => {
+
+  return (
+    <div className={s.container}>
+      <div className={s.divImage}>
+        <img src={product.product.variations[0].mainImage} alt={product.name} className={s.image} />
+      </div>
+      <div className={s.information}>
+        <h3 className={s.name}>{product.name}</h3>
+        <p>{product.id}</p>
+        <p className={s.price}>${product.price}</p>
+        <p className={s.discount}>Paga $2000 con tu compra superior a $40000 en éste fabricante</p>
+      </div>
+    </div>
+  )
+};
+
+
+export default ProductCard;

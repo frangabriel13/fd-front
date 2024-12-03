02b3db6a7cfad5de6846610cf1e8f4fbd428e6c8
Franco <mansilla.franco.1@gmail.com>
2024-12-03 11:38:54 -0300

    cambios

--- src/pages/forgotPassword/ForgotPassword.jsx
+++ src/pages/forgotPassword/ForgotPassword.jsx
@@ -1,9 +1,22 @@
 import s from './ForgotPassword.module.css';
+import { FcGoogle } from "react-icons/fc";
 
 const ForgotPassword = () => {
   return (
-    <div>
-      Forgot Password
+    <div className={s.container}>
+      <div className={s.divTitle}>
+        <h2 className={s.title}>ingresa tu e-mail para recuperar tu contraseña</h2>
+        <p className={s.info}>Se te enviará un correo al e-mail especificado con el enlace para resetear la contraseña</p>
+      </div>
+      <div className={s.divForm}>
+        <form className={s.form}>
+          <div className={s.divInput}>
+            <h3>E-mail</h3>
+            <input type="email" placeholder="Email" className={s.input} />
+          </div>
+          <button type="submit" className={s.button}>Enviar</button>
+        </form>
+      </div>
     </div>
   )
 };

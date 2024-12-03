d5fc8e9be785f8ef656396cd4f9a1710ef586920
Franco <mansilla.franco.1@gmail.com>
2024-12-03 14:28:23 -0300

    cambios

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,22 +1,18 @@
-import s from './Login.module.css';
+import s from './Register.module.css';
 import { FcGoogle } from "react-icons/fc";
 import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Register = () => {
   const navigate = useNavigate();
 
   const handleForgotPassword = () => {
     navigate('/recuperar-password');
   };
 
-  const handleCreateAccount = () => {
-    navigate('/registro');
-  };
-
   return (
     <div className={s.container}>
       <div className={s.divTitle}>
-        <h2 className={s.title}>ingresa tu e-mail y contraseña para iniciar sesión</h2>
+        <h2 className={s.title}>ingresa tu e-mail y contraseña para crear una cuenta</h2>
       </div>
       <div className={s.divForm}>
         <form className={s.form}>
@@ -28,19 +24,16 @@ const Login = () => {
             <h3>Contraseña</h3>
             <input type="password" placeholder="Contraseña" className={s.input} />
           </div>
-          <button type="submit" className={s.button}>Ingresar</button>
+          <div className={s.divInput}>
+            <h3>Repetir contraseña</h3>
+            <input type="password" placeholder="Contraseña" className={s.input} />
+          </div>
+          <button type="submit" className={s.button}>Registrarse</button>
         </form>
-        <div className={s.divBtns}>
-          <button className={s.btnForgot} onClick={handleForgotPassword}>¿Olvidaste tu contraseña?</button>
-          <button className={s.btnCreate} onClick={handleCreateAccount}>Crear cuenta</button>
-          <button className={s.btnGoogle}>
-            <FcGoogle className={s.googleIcon} /> Iniciar sesión con Google
-          </button>
-        </div>
       </div>
     </div>
   );
 };
 
 
-export default Login;
+export default Register;

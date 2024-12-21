08d3e9df4138478573f3900282f3fd1e365f5148
Franco <mansilla.franco.1@gmail.com>
2024-12-21 17:00:23 -0300

    cambios

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,30 @@
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null,
+  error: null,
+}
+
+const authReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+        token: action.payload.token,
+      }
+    case "LOGOUT":
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+        token: null,
+      }
+    default:
+      return state;
+  }
+}
+
+
+export default authReducer;

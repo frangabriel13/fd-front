c9be525dd8931302f1197014d526d815d1072da3
Franco <mansilla.franco.1@gmail.com>
2024-12-23 18:45:12 -0300

    cambios

--- src/pages/resetPassword/ResetPassword.jsx
+++ src/pages/resetPassword/ResetPassword.jsx
@@ -2,13 +2,80 @@ import {  useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { resetPassword } from '../../store/actions/authActions';
 import { useParams, useNavigate } from 'react-router-dom';
+import { resetPasswordValidator } from '../../utils/validations';
 import s from './ResetPassword.module.css';
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const ResetPassword = () => {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [errors, setErrors] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
+  const { token } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const validationErrors = resetPasswordValidator(password, confirmPassword);
+    if(Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    const response = await dispatch(resetPassword(token, password));
+    if(response.success) {
+      setMessage("Constraseña restablecida correctamente. Redirigiendo a la página de inicio de sesión...");
+      setErrors('');
+      setTimeout(() => {
+        navigate('/ingresar');
+      }, 3000);
+    } else {
+      setMessage('');
+      setErrors({ general: response.message }); // o resopnse.error
+    }
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div>
-      ResetPassword
+    <div className={s.container}>
+      <div className={s.divTitle}>
+        <h2 className={s.title}>Ingresa tu nueva contraseña para reestablecerla</h2>
+      </div>
+      <div className={s.divForm}>
+        <form className={s.form} onSubmit={handleSubmit}>
+          <div className={s.divInput}>
+            <h3>Nueva contraseña</h3>
+            <input 
+              type={showPassword ? "text" : "password"}
+              name='password' 
+              placeholder="Contraseña" 
+              className={s.input}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {errors.password && <p className={s.error}>{errors.password}</p>}
+          </div>
+          <div className={s.divInput}>
+            <h3>Repetir contraseña</h3>
+            <input 
+              type={showPassword ? "text" : "password"} 
+              placeholder="Contraseña" 
+              className={s.input}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            {errors.confirmPassword && <p className={s.error}>{errors.confirmPassword}</p>}
+          </div>
+          {errors.general && <p className={s.error}>{errors.general}</p>}
+          <button type="submit" className={s.button}>Restablecer</button>
+        </form>
+        {message && <p className={s.message}>{message}</p>}
+      </div>
     </div>
   )
 };

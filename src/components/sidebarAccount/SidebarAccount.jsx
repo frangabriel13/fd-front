5dba5f788e4438975180a15fa2868c52490c8678
Franco <mansilla.franco.1@gmail.com>
2024-12-28 19:31:45 -0300

    cambios0

--- src/components/sidebarAccount/SidebarAccount.jsx
+++ src/components/sidebarAccount/SidebarAccount.jsx
@@ -1,24 +1,43 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/actions/authActions';
 import s from './SidebarAccount.module.css';
+import { BsUpload, BsBoxSeam, BsTag } from "react-icons/bs";
+import { PiUserCircle } from "react-icons/pi";
+import { CiLogout } from "react-icons/ci";
 
 const SidebarAccount = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     dispatch(logout());
+    setTimeout(() => {
+      navigate('/ingresar');
+    }, 1000);
   };
 
   return (
     <div className={s.container}>
-      <h3 className={s.title}>Mi cuenta</h3>
+      <div className={s.divTitle}>
+        <h3 className={s.title}>Mi cuenta</h3>
+      </div>
       <div className={s.menu}>
-        <Link to="/mi-cuenta/subir-producto" className={s.link}>Subir producto</Link>
-        <Link to="/mi-cuenta/publicaciones" className={s.link}>Publicaciones</Link>
-        <Link to="/mi-cuenta/ordenes" className={s.link}>Mis ordenes</Link>
-        <Link to="/mi-cuenta/perfil" className={s.link}>Mis perfil</Link>
-        <button onClick={handleLogout} className={s.link}>Cerrar sesión</button>
+        <Link to="/mi-cuenta/subir-producto" className={s.link}>
+          <BsUpload className={s.icon} /> Subir producto
+        </Link>
+        <Link to="/mi-cuenta/publicaciones" className={s.link}>
+          <BsBoxSeam className={s.icon} /> Publicaciones
+        </Link>
+        <Link to="/mi-cuenta/ordenes" className={s.link}>
+          <BsTag className={s.icon} /> Mis ordenes
+        </Link>
+        <Link to="/mi-cuenta/perfil" className={s.link}>
+          <PiUserCircle className={s.icon} /> Mi perfil
+        </Link>
+        <button onClick={handleLogout} className={s.btnLogout}>
+          <CiLogout className={s.iconLogout} /> Cerrar sesión
+        </button>
       </div>
     </div>
   );

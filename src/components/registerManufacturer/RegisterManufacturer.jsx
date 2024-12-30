eca5c87c09ac68bbfee5ff356eadcd08ff59c7c1
Franco <mansilla.franco.1@gmail.com>
2024-12-30 16:39:45 -0300

    cambios

--- src/components/registerManufacturer/RegisterManufacturer.jsx
+++ src/components/registerManufacturer/RegisterManufacturer.jsx
@@ -3,9 +3,45 @@ import s from './RegisterManufacturer.module.css';
 const RegisterManufacturer = () => {
   return (
     <div className={s.container}>
-      <h2>
-        Register Manufacturer
-      </h2>
+      <h3>Datos del Fabricante</h3>
+      <form className={s.form}>
+        <div className={s.divInputs}>
+          <div className={s.divInput}>
+            <h4>Nombre del fabricante</h4>
+            <input className={s.input} type="text" />
+          </div>
+          <div className={s.divInput}>
+            <h4>Nombre de la tienda</h4>
+            <input className={s.input} type="text" />
+          </div>
+        </div>
+        <div className={s.divInputs}>
+          <div className={s.divInput}>
+            <h4>Número de teléfono</h4>
+            <input className={s.input} type="text" />
+          </div>
+          <div className={s.divInput}>
+            <h4>Mínimo de compra</h4>
+            <input className={s.input} type="text" />
+          </div>
+        </div>
+        <div className={s.divInputAddress}>
+          <div className={s.divInputCheck}>
+            <h4>¿Cuénta con punto de venta?</h4>
+            <input 
+              className={s.inputCheck}
+              type="checkbox"
+            />
+          </div>
+          <div className={s.inputAddress}>
+            <h4>Dirección</h4>
+            <input className={s.input} type="text" />
+          </div>
+        </div>
+        <div className={s.divBtn}>
+          <button className={s.btnForm}>Completar Registro</button>
+        </div>
+      </form>
     </div>
   )
 };

5e18aec321675f524cb5d885cd2455b5b784cb42
Franco <mansilla.franco.1@gmail.com>
2024-12-30 12:56:17 -0300

    cambios

---
+++ src/components/registerWholesaler/RegisterWholesaler.jsx
@@ -0,0 +1,14 @@
+import s from './RegisterWholesaler.module.css';
+
+const RegisterWholesaler = () => {
+  return (
+    <div>
+      <h2>
+        Register Wholesaler
+      </h2>
+    </div>
+  )
+};
+
+
+export default RegisterWholesaler;
--- src/pages/completeRegistration/CompleteRegistration.jsx
+++ src/pages/completeRegistration/CompleteRegistration.jsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
+import RegisterWholesaler from '../../components/registerWholesaler/RegisterWholesaler';
+import RegisterManufacturer from '../../components/registerManufacturer/RegisterManufacturer';
 import s from './CompleteRegistration.module.css';
 
 const CompleteRegistration = () => {
+  const [role, setRole] = useState('');
+
+  const handleRoleSelection = (role) => {
+    setRole(role);
+  };
+
   return (
     <div className={s.container}>
       <div>
@@ -8,9 +17,15 @@ const CompleteRegistration = () => {
         <p>Por favor, completa tu registro para poder acceder a todas las funcionalidades de la aplicación.</p>
       </div>
       <div className={s.divOptions}>
-        <button>Fabricante</button>
-        <button>Mayorista</button>
+        <button className={s.btnOption} onClick={() => handleRoleSelection('manufacturer')}>Fabricante</button>
+        <button className={s.btnOption} onClick={() => handleRoleSelection('wholesaler')}>Mayorista</button>
       </div>
+      {
+        role === 'manufacturer' && <RegisterManufacturer />
+      }
+      {
+        role === 'wholesaler' && <RegisterWholesaler />
+      }
     </div>
   )
 };

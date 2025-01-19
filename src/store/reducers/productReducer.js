e6069cf82f665629d4457cb578993fcde2e198ce
Franco <mansilla.franco.1@gmail.com>
2025-01-19 18:07:31 -0300

    cambios

--- src/store/reducers/productReducer.js
+++ src/store/reducers/productReducer.js
@@ -1,5 +1,6 @@
 const initialState = {
   products: [],
+  myProducts: [],
   currentPage: 1,
   pageSize: 20,
   totalProducts: 0,
@@ -22,6 +23,12 @@ const productReducer = (state = initialState, action) => {
         totalProducts: action.payload.totalProducts,
       };
     case 'GET_PRODUCTS_FAILURE':
+      return { ...state, loading: false, error: action.error };
+    case 'GET_PRODUCTS_BY_USER_REQUEST':
+      return { ...state, loading: true, error: null };
+    case 'GET_PRODUCTS_BY_USER_SUCCESS':
+      return { ...state, loading: false, error: null, myProducts: action.payload };
+    case 'GET_PRODUCTS_BY_USER_FAILURE':
       return { ...state, loading: false, error: action.error };
     case 'CREATE_PRODUCT_REQUEST':
       return { ...state, loading: true, error: null };

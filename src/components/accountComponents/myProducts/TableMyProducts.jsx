b73720535ca91fdd62713b402d75c7bd9b9dea70
Franco <mansilla.franco.1@gmail.com>
2025-01-21 18:52:16 -0300

    cambios

--- src/components/accountComponents/myProducts/TableMyProducts.jsx
+++ src/components/accountComponents/myProducts/TableMyProducts.jsx
@@ -1,14 +1,42 @@
 import s from './TableMyProducts.module.css';
 
 const TableMyProducts = ({ myProducts, handleEdit, handleDelete }) => {
+  console.log(myProducts);
   return (
     <div className={s.container}>
       <h3>Mis Productos</h3>
       <div className={s.divData}>
-        <div className={s.divInput}>
-          <h4>Email</h4>
-          <p>Text</p>
+        <div className={s.divSearch}>
+          <h4>Search</h4>
+          <p>Aca va el search</p>
         </div>
+        <table className={s.table}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Nombre</th>
+              <th>Tipo</th>
+              <th>Categoría</th>
+              <th>Género</th>
+              <th>Precio</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              myProducts.map(product => (
+                <tr key={product.id}>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.stock}</td>
+                  <td>
+                    <button onClick={() => handleEdit(product.id)}>Editar</button>
+                    <button onClick={() => handleDelete(product.id)}>Eliminar</button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </div>
     </div>
   );

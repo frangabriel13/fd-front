2607286d3670a3eb4478a75a0c06fca7fff74897
Franco <mansilla.franco.1@gmail.com>
2025-01-22 10:36:28 -0300

    cambios

--- src/components/accountComponents/myProducts/TableMyProducts.jsx
+++ src/components/accountComponents/myProducts/TableMyProducts.jsx
@@ -10,33 +10,42 @@ const TableMyProducts = ({ myProducts, handleEdit, handleDelete }) => {
           <h4>Search</h4>
           <p>Aca va el search</p>
         </div>
-        <table className={s.table}>
-          <thead>
-            <tr>
-              <th></th>
-              <th>Nombre</th>
-              <th>Tipo</th>
-              <th>Categoría</th>
-              <th>Género</th>
-              <th>Precio</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              myProducts.map(product => (
-                <tr key={product.id}>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>{product.stock}</td>
-                  <td>
-                    <button onClick={() => handleEdit(product.id)}>Editar</button>
-                    <button onClick={() => handleDelete(product.id)}>Eliminar</button>
-                  </td>
-                </tr>
-              ))
-            }
-          </tbody>
-        </table>
+        <div className={s.divProducts}>
+
+          <table className={s.table}>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Nombre</th>
+                <th>Tipo</th>
+                <th>Categoría</th>
+                <th>Género</th>
+                <th>ARS</th>
+                <th>USD</th>
+                <th className={s.thActions}>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                myProducts.map(product => (
+                  <tr key={product.id}>
+                    <td><img src={product.mainImage} alt={product.name} /></td>
+                    <td>{product.name}</td>
+                    <td>{product.type}</td>
+                    <td>{product.category.name}</td>
+                    <td>{product.gender.name}</td>
+                    <td>{product.price}</td>
+                    <td>{product.priceDolar}</td>
+                    <td className={s.tdActions}>
+                      <button onClick={() => handleEdit(product.id)}>Editar</button>
+                      <button onClick={() => handleDelete(product.id)}>Eliminar</button>
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );

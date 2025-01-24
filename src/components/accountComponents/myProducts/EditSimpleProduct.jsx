70e7eedbeefc445eca6c151f0eb803714960fb77
Franco <mansilla.franco.1@gmail.com>
2025-01-24 14:02:35 -0300

    cambios

---
+++ src/components/accountComponents/myProducts/EditSimpleProduct.jsx
@@ -0,0 +1,12 @@
+import s from './EditSimpleProduct.module.css';
+
+const EditSimpleProduct = ({ product, handleEdit, handleDelete }) => {
+  return (
+    <div>
+      <h2>Editando</h2>
+    </div>
+  );
+};
+
+
+export default EditSimpleProduct;

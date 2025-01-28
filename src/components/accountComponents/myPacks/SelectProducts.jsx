dede1a8a9a3fef3e265964061460bcbaa2db2e33
Franco <mansilla.franco.1@gmail.com>
2025-01-28 16:24:28 -0300

    cambios

--- src/components/accountComponents/myPacks/SelectProducts.jsx
+++ src/components/accountComponents/myPacks/SelectProducts.jsx
@@ -1,10 +1,24 @@
 import s from './SelectProducts.module.css';
+import { formatPrice } from '../../../utils/utils';
 
 const SelectProducts = ({ myProducts, selectedProducts, onClose, onSelect }) => {
   const handleClickOutside = (e) => {
     if (e.target === e.currentTarget) {
       onClose();
     }
+  };
+
+  const handleCheckboxChange = (product) => {
+    const isSelected = selectedProducts.some(selected => selected.id === product.id);
+    let updatedSelectedProducts;
+
+    if(isSelected) {
+      updatedSelectedProducts = selectedProducts.filter(selected => selected.id !== product.id);
+    } else {
+      updatedSelectedProducts = [...selectedProducts, product];
+    }
+
+    onSelect(updatedSelectedProducts);
   };
 
   return (
@@ -14,6 +28,31 @@ const SelectProducts = ({ myProducts, selectedProducts, onClose, onSelect }) => 
           <div className={s.divTitle}>
             <h3>Seleccionar productos</h3>
             <p>Selecciona los productos que deseas agregar a tu pack de emprendedor</p>
+          </div>
+          <div className={s.divList}>
+            {myProducts.map(product => (
+              <div key={product.id} className={s.productItem}>
+                <div className={s.dataProduct}>
+                  <img src={product.mainImage} alt={product.name} className={s.productImage} />
+                  <div className={s.productInfo}>
+                    <h4>{product.name}</h4>
+                    <p>{formatPrice(product.price)}</p>
+                  </div>
+                </div>
+                <input
+                  type="checkbox"
+                  checked={selectedProducts.some(selected => selected.id === product.id)}
+                  onChange={() => handleCheckboxChange(product)}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={s.divDivider}>
+          <hr className={s.divider} />
+          <div className={s.divBtn}>
+            <button className={s.btnCancel} onClick={onClose}>Cerrar</button>
+            <button className={s.btnNext} type='submit'>Guardar</button>
           </div>
         </div>
       </div>

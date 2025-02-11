2088940e3c21a4d234cbc9eb006e1a3b7aba878d
Franco <mansilla.franco.1@gmail.com>
2025-02-11 15:58:29 -0300

    cambios

--- src/pages/store/Store.jsx
+++ src/pages/store/Store.jsx
@@ -1,12 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { getUserData } from '../../store/actions/storeActions';
 import s from './Store.module.css';
 
 const Store = () => {
-  const { manufacturerId } = useParams();
+  const { userId } = useParams();
+  const dispatch = useDispatch();
+  const { manufacturer } = useSelector(state => state.manufacturer);
+  const { manufacturerProducts } = useSelector(state => state.product);
+
+  console.log(manufacturer);
+  console.log(manufacturerProducts);
+
+  useEffect(() => {
+    dispatch(getUserData(userId));
+  }, [dispatch, userId]);
 
   return (
     <div className={s.store}>
-      <h1>Tienda del fabricante {manufacturerId}</h1>
+      <h1>Tienda del fabricante {userId}</h1>
     </div>
   );
 };

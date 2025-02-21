5f7ee3b10520428afdceec447f8fb3143b93dd00
Franco <mansilla.franco.1@gmail.com>
2025-02-21 15:36:53 -0300

    cambios

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,16 +1,33 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProducts } from '../../store/actions/productActions';
 import s from './Shop.module.css';
+import ProductCard from '../../components/productStore/ProductCard';
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  // pageSize no es un estado global
+  const { products, currentPage, totalProducts } = useSelector(state => state.product);
+
+  useEffect(() => {
+    dispatch(getProducts(currentPage));
+  }, [dispatch, currentPage]);
+
   return (
     <div className={s.container}>
-      <h1>Shop</h1>
-      <h1>Shop</h1>
-      <h1>Shop</h1>
-      <h1>Shop</h1>
-      <h1>Shop</h1>
-      <h1>Shop</h1>
-      <h1>Shop</h1>
-      <h1>Shop</h1>
+      <div className={s.divHeader}>
+        <h2 className={s.title}>Tienda</h2>
+      </div>
+      <div className={s.divProducts}>
+        {products.map(product => (
+          <ProductCard 
+            key={product.id} 
+            name={product.name}
+            image={product.mainImage}
+            price={product.price}
+          />
+        ))}
+      </div>
     </div>
   );
 };

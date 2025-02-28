a4cfba18034712bb3cc1586b9bbcf0e4fd15a780
Franco <mansilla.franco.1@gmail.com>
2025-02-28 09:26:38 -0300

    cambios

---
+++ src/components/detailProduct/DataProduct.jsx
@@ -0,0 +1,12 @@
+import s from './DataProduct.module.css';
+
+const DataProduct = ({ product, manufacturer }) => {
+  return (
+    <div className={s.container}>
+      data prod
+    </div>
+  );
+};
+
+
+export default DataProduct;

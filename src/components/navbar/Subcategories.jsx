665d9aad6281c2fcbe2fbce7761bda8368f480a9
Franco <mansilla.franco.1@gmail.com>
2025-03-01 14:23:27 -0300

    cambios

--- src/components/navbar/Subcategories.jsx
+++ src/components/navbar/Subcategories.jsx
@@ -3,7 +3,13 @@ import s from './Subcategories.module.css';
 const Subcategories = ({ categories }) => {
   return(
     <div className={s.container}>
-      <h5>{categories[0].parent.name}</h5>
+      <h5 className={s.title}>{categories[0].parent.name}</h5>
+      <hr className={s.divider} />
+      <ul className={s.ul}>
+        {categories.map(category => (
+          <li key={category.id} className={s.li}>{category.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };

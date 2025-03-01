57df563a0061695d36828747ff76fefef5c7006e
Franco <mansilla.franco.1@gmail.com>
2025-03-01 17:29:49 -0300

    camibos

--- src/store/reducers/manufacturerReducer.js
+++ src/store/reducers/manufacturerReducer.js
@@ -1,6 +1,9 @@
 const initialState = {
   liveManufacturers: [],
   manufacturers: [],
+  currentPage: 1,
+  pageSize: 18,
+  totalManufacturers: 0,
   manufacturer: {},
   loading: false,
   error: null,
@@ -9,6 +12,28 @@ const initialState = {
 
 const manufacturerReducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'GET_MANUFACTURERS_REQUEST':
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case 'GET_MANUFACTURERS':
+      return {
+        ...state,
+        manufacturers: action.payload.manufacturers,
+        currentPage: action.payload.currentPage,
+        pageSize: action.payload.pageSize,
+        totalManufacturers: action.payload.totalManufacturers,
+        loading: false,
+        error: null,
+      };
+    case 'GET_MANUFACTURERS_FAILURE':
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
     case 'GET_LIVE_MANUFACTURERS_REQUEST':
       return {
         ...state,

280f7c2c1b370613611ed91d4611827645402fcd
Franco <mansilla.franco.1@gmail.com>
2025-03-01 13:19:09 -0300

    cambios

--- src/components/navbar/Dropdown.jsx
+++ src/components/navbar/Dropdown.jsx
@@ -1,22 +1,38 @@
+import { useState } from 'react';
 import s from './Dropdown.module.css';
 import { GrNext } from "react-icons/gr";
+import { filterCategoriesByParentAndGender } from '../../utils/utils';
+import Subcategories from './Subcategories';
 
 const Dropdown = ({ categories }) => {
+  const [hoveredCategory, setHoveredCategory] = useState(null);
+
+  const handleMouseEnter = (parentId) => {
+    setHoveredCategory(parentId);
+  }
+
+  const handleMouseLeave = () => {
+    setHoveredCategory(null);
+  }
+
   return (
     <div className={s.container}>
       <div className={s.divCategories}>
-        <div className={s.divCategory}>
+        <div className={s.divCategory} onMouseEnter={() => handleMouseEnter(1)} onMouseLeave={handleMouseLeave}>
           Indumentaria
           <GrNext className={s.icon} />
         </div>
-        <div className={s.divCategory}>
+        <div className={s.divCategory} onMouseEnter={() => handleMouseEnter(2)} onMouseLeave={handleMouseLeave}>
           Bisutería
           <GrNext className={s.icon} />
         </div>
-        <div className={s.divCategory}>
+        <div className={s.divCategory} onMouseEnter={() => handleMouseEnter(3)} onMouseLeave={handleMouseLeave}>
           Blanquería
           <GrNext className={s.icon} />
         </div>
+      </div>
+      <div className={s.divSubcategories}>
+        {hoveredCategory && <Subcategories categories={filterCategoriesByParentAndGender(categories, hoveredCategory, null)} />}
       </div>
     </div>
   );

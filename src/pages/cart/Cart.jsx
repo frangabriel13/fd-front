85beb255fa49c673db78aa72863e990e4f740df8
Franco <mansilla.franco.1@gmail.com>
2025-04-10 16:06:47 -0300

    camibos

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import s from "./Cart.module.css"
 
 const Cart = () => {
+  const { items } = useSelector((state) => state.cart);
+  console.log('items', items);
+
   return(
     <div className={s.container}>
       Este es mi carrito de compras

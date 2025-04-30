229e56fbe9e68f51316c31e14b8c83bfd2e1bc1d
Franco <mansilla.franco.1@gmail.com>
2025-04-30 19:39:42 -0300

    cambios

--- src/store/actions/orderActions.js
+++ src/store/actions/orderActions.js
@@ -1,18 +1,19 @@
 import { orderInstance } from "../../utils/axiosConfig";
 
 export const createOrder = (orderData) => async (dispatch) => {
-  console.log('orderData', orderData);
-  // dispatch({ type: 'CREATE_ORDER_REQUEST' });
-  // try {
-  //   const response = await orderInstance.post('/', orderData);
-  //   dispatch({
-  //     type: 'CREATE_ORDER_SUCCESS',
-  //     payload: response.data,
-  //   });
-  // } catch (error) {
-  //   dispatch({
-  //     type: 'CREATE_ORDER_FAILURE',
-  //     error: error.message,
-  //   });
-  // }
+  console.log('Creating order with data:', orderData);
+  dispatch({ type: 'CREATE_ORDER_REQUEST' });
+  try {
+    const response = await orderInstance.post('/', orderData);
+    console.log('Order created successfully:', response.data);
+    // dispatch({
+    //   type: 'CREATE_ORDER_SUCCESS',
+    //   payload: response.data,
+    // });
+  } catch (error) {
+    dispatch({
+      type: 'CREATE_ORDER_FAILURE',
+      error: error.message,
+    });
+  }
 };

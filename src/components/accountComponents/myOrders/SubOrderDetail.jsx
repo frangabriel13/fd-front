67bc220a6605cd6d198315ea383b7b626609f2c6
Franco <mansilla.franco.1@gmail.com>
2025-05-05 09:36:51 -0300

    camb ios

--- src/components/accountComponents/myOrders/SubOrderDetail.jsx
+++ src/components/accountComponents/myOrders/SubOrderDetail.jsx
@@ -1,4 +1,5 @@
 import s from './SubOrderDetail.module.css';
+import { formatPrice } from '../../../utils/utils';
 
 const SubOrderDetail = ({ subOrder, onClose }) => {
   const handleClickOutside = (e) => {
@@ -23,17 +24,33 @@ const SubOrderDetail = ({ subOrder, onClose }) => {
                   <thead>
                     <tr>
                       <th>Nombre</th>
+                      <th>Talle</th>
+                      <th>Color</th>
+                      <th>Cantidad</th>
                       <th>Precio</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {subOrder.products.map((product, index) => (
-                      <tr key={index}>
-                        <td>{product.name}</td>
-                        <td>{product.price}</td>
-                      </tr>
-                    ))}
-                  </tbody>
+        {subOrder.products.map((product, index) => (
+          product.inventories && product.inventories.length > 0 ? (
+            product.inventories.map((inventory, invIndex) => (
+              <tr key={`${index}-${invIndex}`}>
+                <td>{product.name}</td>
+                <td>{inventory.size}</td>
+                <td>{inventory.color}</td>
+                <td>{inventory.totalItem}</td>
+                <td>{formatPrice(product.price)}</td>
+              </tr>
+            ))
+          ) : (
+            <tr key={index}>
+              <td>{product.name}</td>
+              <td colSpan="3">Sin inventarios</td>
+              <td>{formatPrice(product.price)}</td>
+            </tr>
+          )
+        ))}
+      </tbody>
                 </table>
               </div>
             )}
@@ -44,6 +61,7 @@ const SubOrderDetail = ({ subOrder, onClose }) => {
                   <thead>
                     <tr>
                       <th>Nombre</th>
+                      <th>Cantidad</th>
                       <th>Precio</th>
                     </tr>
                   </thead>
@@ -51,7 +69,8 @@ const SubOrderDetail = ({ subOrder, onClose }) => {
                     {subOrder.packs.map((pack, index) => (
                       <tr key={index}>
                         <td>{pack.name}</td>
-                        <td>{pack.price}</td>
+                        <td>{pack.totalItem}</td>
+                        <td>{formatPrice(pack.price)}</td>
                       </tr>
                     ))}
                   </tbody>

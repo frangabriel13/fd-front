b6fee283c8f4367225af0b58a0fb24f38738055f
Franco <mansilla.franco.1@gmail.com>
2025-05-06 17:34:05 -0300

    cambios

--- src/components/accountComponents/myPurchases/OrderDetail.jsx
+++ src/components/accountComponents/myPurchases/OrderDetail.jsx
@@ -18,71 +18,76 @@ const OrderDetail = ({ order, onClose }) => {
           </div>
           <div className={s.divTable}>
             {order.subOrders && order.subOrders.length > 0 && order.subOrders.map((subOrder, index) => (
-              <div key={index} className={s.tableSection}>
+              <div key={index} className={s.divSuborder}>
                 <h4>{subOrder.user.manufacturer.name} - suborden #{subOrder.id}</h4>
-                {subOrder.products && subOrder.products.length > 0 && (
-                  <div className={s.tableSection}>
-                    <h4>Productos</h4>
-                    <table className={s.table}>
-                      <thead>
-                        <tr>
-                          <th>Nombre</th>
-                          <th>Talle</th>
-                          <th>Color</th>
-                          <th>Cantidad</th>
-                          <th>Precio</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {subOrder.products.map((product, index) => (
-                          product.inventories && product.inventories.length > 0 ? (
-                            product.inventories.map((inventory, invIndex) => (
-                              <tr key={`${index}-${invIndex}`}>
+                <div className={s.divProdPack}>
+                  {subOrder.products && subOrder.products.length > 0 && (
+                    <div className={s.tableSection}>
+                      <h4>Productos</h4>
+                      <table className={s.table}>
+                        <thead>
+                          <tr>
+                            <th>Nombre</th>
+                            <th>Talle</th>
+                            <th>Color</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {subOrder.products.map((product, index) => (
+                            product.inventories && product.inventories.length > 0 ? (
+                              product.inventories.map((inventory, invIndex) => (
+                                <tr key={`${index}-${invIndex}`}>
+                                  <td>{product.name}</td>
+                                  <td>{inventory.size}</td>
+                                  <td>{inventory.color}</td>
+                                  <td>{inventory.totalItem}</td>
+                                  <td>{formatPrice(product.price)}</td>
+                                </tr>
+                              ))
+                            ) : (
+                              <tr key={index}>
                                 <td>{product.name}</td>
-                                <td>{inventory.size}</td>
-                                <td>{inventory.color}</td>
-                                <td>{inventory.totalItem}</td>
+                                <td colSpan="3">Sin inventarios</td>
                                 <td>{formatPrice(product.price)}</td>
                               </tr>
-                            ))
-                          ) : (
-                            <tr key={index}>
-                              <td>{product.name}</td>
-                              <td colSpan="3">Sin inventarios</td>
-                              <td>{formatPrice(product.price)}</td>
-                            </tr>
-                          )
-                        ))}
-                      </tbody>
-                    </table>
-                  </div>
-                )}
-                {subOrder.packs && subOrder.packs.length > 0 && (
-                  <div className={s.tableSection}>
-                    <h4>Packs</h4>
-                    <table className={s.table}>
-                      <thead>
-                        <tr>
-                          <th>Nombre</th>
-                          <th>Cantidad</th>
-                          <th>Precio</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {subOrder.packs.map((pack, index) => (
-                          <tr key={index}>
-                            <td>{pack.name}</td>
-                            <td>{pack.totalItem}</td>
-                            <td>{formatPrice(pack.price)}</td>
+                            )
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  )}
+                  {subOrder.packs && subOrder.packs.length > 0 && (
+                    <div className={s.tableSection}>
+                      <h4>Packs</h4>
+                      <table className={s.table}>
+                        <thead>
+                          <tr>
+                            <th>Nombre</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
                           </tr>
-                        ))}
-                      </tbody>
-                    </table>
-                  </div>
-                )}
+                        </thead>
+                        <tbody>
+                          {subOrder.packs.map((pack, index) => (
+                            <tr key={index}>
+                              <td>{pack.name}</td>
+                              <td>{pack.totalItem}</td>
+                              <td>{formatPrice(pack.price)}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  )}
+                </div>
               </div>
             ))}
           </div>
+        </div>
+        <div className={s.divActions}>
+          <button className={s.btnCancel} onClick={onClose}>Cancelar</button>
         </div>
       </div>
     </div>

ede6c8d77eeaee936ac6d1cadc0aa8e1e42f4df4
Franco <mansilla.franco.1@gmail.com>
2025-05-11 13:53:34 -0300

    cambios

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -2,18 +2,36 @@ import { useState } from 'react';
 import { BsStar, BsStarFill, BsStarHalf } from 'react-icons/bs';
 import s from './Reviews.module.css';
 import Review from './Review';
+import CreateReview from './CreateReview';
 
-const Reviews = ({ reviews }) => {
-  console.log('reviews', reviews);
+const Reviews = ({ reviews, manufacturerId }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className={s.container}>
-      <h2>Reviews</h2>
+      <h2>Últimas reviews</h2>
       <div className={s.container}>
+        <div>
+          <button onClick={handleOpenModal}>Hacer una review</button>
+        </div>
         {reviews.map((review, index) => (
           <Review key={index} review={review} />
         ))}
       </div>
+      {isModalOpen && (
+        <CreateReview
+          manufacturerId={manufacturerId}
+          onClose={handleCloseModal}
+        />
+      )}
     </div>
   )
 };

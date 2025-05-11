1c5bd0082539aa178340d1af41c7565ea43a2dd7
Franco <mansilla.franco.1@gmail.com>
2025-05-11 12:30:32 -0300

    cambios

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react';
 import { BsStar, BsStarFill, BsStarHalf } from 'react-icons/bs';
 import s from './Reviews.module.css';
+import Review from './Review';
 
-const Reviews = () => {
+const Reviews = ({ reviews }) => {
+  console.log('reviews', reviews);
+
   return (
-    <div>lalalalala</div>
+    <div className={s.container}>
+      <h2>Reviews</h2>
+      <div className={s.container}>
+        {reviews.map((review, index) => (
+          <Review key={index} review={review} />
+        ))}
+      </div>
+    </div>
   )
 };
 

5f98621079c60b4e6e041dff4e98d894069f5300
Franco <mansilla.franco.1@gmail.com>
2025-05-11 14:34:45 -0300

    cambios

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { BsStar, BsStarFill, BsStarHalf } from 'react-icons/bs';
 import s from './Reviews.module.css';
 import Review from './Review';
 import CreateReview from './CreateReview';
+import { createReview } from '../../store/actions/reviewActions';
 
 const Reviews = ({ reviews, manufacturerId }) => {
+  const dispatch = useDispatch();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpenModal = () => {
@@ -14,6 +17,16 @@ const Reviews = ({ reviews, manufacturerId }) => {
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
+
+  const handleCreateReview = async (reviewData) => {
+    const response = await dispatch(createReview(manufacturerId, reviewData));
+    if(response.success) {
+      // console.log('Reseña creada con éxito:', response.data);
+      setIsModalOpen(false);
+    } else {
+      console.error('Error al crear la reseña:', response.error);
+    }
+  }
 
   return (
     <div className={s.container}>
@@ -30,6 +43,7 @@ const Reviews = ({ reviews, manufacturerId }) => {
         <CreateReview
           manufacturerId={manufacturerId}
           onClose={handleCloseModal}
+          onSubmit={handleCreateReview}
         />
       )}
     </div>

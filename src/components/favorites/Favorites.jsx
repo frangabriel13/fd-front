9edce33726c7c8b7dfda274954ae0be734f9ecd7
Franco <mansilla.franco.1@gmail.com>
2025-05-13 16:57:19 -0300

    cambios

--- src/components/favorites/Favorites.jsx
+++ src/components/favorites/Favorites.jsx
@@ -1,10 +1,25 @@
 import s from './Favorites.module.css';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getFavorites, deleteFavorite } from '../../store/actions/favoriteActions';
 import TableFavorites from './TableFavorites';
 
 const Favorites = () => {
+  const dispatch = useDispatch();
+  const { favorites } = useSelector((state) => state.favorite);
+
+  useEffect(() => {
+    dispatch(getFavorites());
+  }, [dispatch]);
+
+  const handleDelete = (productId) => {
+    dispatch(deleteFavorite(productId));
+  };
+
+  console.log('favorites', favorites);
   return (
     <div className={s.container}>
-      <TableFavorites />
+      <TableFavorites favorites={favorites} onDelete={handleDelete} />
     </div>
   );
 };
--- src/components/favorites/TableFavorites.jsx
+++ src/components/favorites/TableFavorites.jsx
@@ -1,13 +1,39 @@
+import { useNavigate } from 'react-router-dom';
 import s from './TableFavorites.module.css';
+import { formatPrice } from '../../utils/utils';
 
-const TableFavorites = () => {
+const TableFavorites = ({ favorites, onDelete }) => {
+  const navigate = useNavigate();
+
+  const handleViewProduct = (productId) => {
+    navigate(`/producto/${productId}`);
+  };
+
   return (
     <div className={s.container}>
       <h3>Mis favoritos</h3>
-      <div className={s.divData}>
-        <div className={s.divFavorites}>
-          <table className={s.table}></table>
-        </div>
+      <div className={s.divFavorites}>
+        {favorites.map((item, index) => (
+          <div key={index} className={s.favoriteItem}>
+            <div className={s.divItem}>
+              <img src={item.mainImage} alt={item.name} className={s.image} />
+              <div className={s.divData}>
+                <p>{item.name}</p>
+                <p>{formatPrice(item.price)}</p>
+              </div>
+            </div>
+            <div className={s.divActions}>
+              <button 
+                onClick={() => onDelete(item.productId)} 
+                className={s.buttonDelete}
+              >Eliminar</button>
+              <button 
+                className={s.buttonView} 
+                onClick={() => handleViewProduct(item.productId)}
+              >Ver producto</button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )

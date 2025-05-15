18fe513cca67cd1534e890abad252ab22a12080c
Franco <mansilla.franco.1@gmail.com>
2025-05-15 13:56:55 -0300

    cambios

---
+++ src/components/accountComponents/users/TableUsers.jsx
@@ -0,0 +1,10 @@
+import s from "./TableUsers.module.css";
+
+const TableUsers = ({ users }) => {
+  return(
+    <div>tabla</div>
+  );
+};
+
+
+export default TableUsers;
--- src/components/accountComponents/users/Users.jsx
+++ src/components/accountComponents/users/Users.jsx
@@ -1,10 +1,10 @@
+import TableUsers from "./TableUsers";
 import s from "./Users.module.css";
 
 const Users = () => {
   return (
     <div className={s.container}>
-      <h1>Users</h1>
-      <p>Users component content goes here.</p>
+      <TableUsers />
     </div>
   );
 };

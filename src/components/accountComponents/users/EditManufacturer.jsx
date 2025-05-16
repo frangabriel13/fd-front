803e2b7801ea298f6302329884636910f7220a15
Franco <mansilla.franco.1@gmail.com>
2025-05-16 13:19:30 -0300

    cambios

--- src/components/accountComponents/users/EditManufacturer.jsx
+++ src/components/accountComponents/users/EditManufacturer.jsx
@@ -2,7 +2,39 @@ import s from './EditManufacturer.module.css';
 
 const EditManufacturer = ({ manufacturer, onClose }) => {
   return (
-    <div>editando</div>
+    <div className={s.modal}>
+      <div className={s.modalContent}>
+        <div className={s.container}>
+          <h3>Editar información</h3>
+          <form className={s.form}>
+            <div className={s.divInputAddress}>
+              <div className={s.divInputCheck}>
+                <h4>¿Cuénta con punto de venta?</h4>
+                <input
+                  className={s.inputCheck}
+                  type="checkbox"
+                  name="pointOfSale"
+                />
+              </div>
+              <div className={s.inputAddress}>
+                <h4>Dirección</h4>
+                <input
+                className={s.input}
+                  // className={`${s.input} ${!formData.pointOfSale ? s.inputDisabled : ''}`}
+                  type="text"
+                  name="street"
+                />
+                {/* {errors.street && <p className={s.error}>{errors.street}</p>} */}
+              </div>
+            </div>
+            <div className={s.divBtn}>
+              <button className={s.btnCancel} onClick={onClose}>Cancelar</button>
+              <button className={s.btnForm} type='submit'>Guardar cambios</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 };
 

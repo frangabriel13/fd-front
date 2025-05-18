ab9736b276274b46989202ad74ea0c9cecea7327
Franco <mansilla.franco.1@gmail.com>
2025-05-18 12:52:06 -0300

    cambios

---
+++ src/components/navbar/NavbarMobile.jsx
@@ -0,0 +1,12 @@
+import s from './NavbarMobile.module.css';
+
+const NavbarMobile = () => {
+  return (
+    <div className={s.container}>
+      uhuhuhuh
+    </div>
+  );
+};
+
+
+export default NavbarMobile;

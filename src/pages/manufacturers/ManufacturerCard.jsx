7304f8c44c0ec685e049282d5a33b2990df7ce93
Franco <mansilla.franco.1@gmail.com>
2025-05-27 17:17:42 -0300

    cambios

--- src/pages/manufacturers/ManufacturerCard.jsx
+++ src/pages/manufacturers/ManufacturerCard.jsx
@@ -4,14 +4,23 @@ import s from './ManufacturerCard.module.css';
 const ManufacturerCard = ({ manufacturer }) => {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate(`/store/${manufacturer.userId}`);
+  const handleCardClick = () => {
+    navigate(`/store/${manufacturer.user.id}`);
   };
 
+  const handleImageClick = (e) => {
+    if (manufacturer.live) {
+      e.stopPropagation();
+      window.open(manufacturer.tiktokUrl, '_blank');
+    } else {
+      // Si no está en vivo, deja que el click siga su curso y navegue a store
+      // No hacemos nada aquí
+    }
+  };
   return (
-    <div className={s.container}>
+    <div className={s.container} onClick={handleCardClick}>
       <div className={s.divImage}>
-        <img src={manufacturer.image} alt={manufacturer.name} onClick={handleClick} />
+        <img src={manufacturer.image} alt={manufacturer.name} onClick={handleImageClick} />
         {manufacturer.live && <div className={s.live}>LIVE</div>}
       </div>
       <h3>{manufacturer.name}</h3>

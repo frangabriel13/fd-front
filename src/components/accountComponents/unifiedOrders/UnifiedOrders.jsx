edab55696a7b88e66b4c418a54bbed93b74f96c6
Franco <mansilla.franco.1@gmail.com>
2025-05-29 15:49:56 -0300

    cambios

--- src/components/accountComponents/unifiedOrders/UnifiedOrders.jsx
+++ src/components/accountComponents/unifiedOrders/UnifiedOrders.jsx
@@ -1,16 +1,30 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { getAllUnifiedOrders } from '../../../store/actions/unifiedOrderActions';
+import { getUnifiedOrders } from '../../../store/actions/orderActions';
 import TableUnified from './TableUnified';
 import s from './UnifiedOrders.module.css';
 
 const UnifiedOrders = () => {
   const dispatch = useDispatch();
+  const { unifiedOrders = [], total, totalPages, page } = useSelector((state) => state.order);
+
+  useEffect(() => {
+    dispatch(getUnifiedOrders(page));
+  }, [dispatch, page]);
+
+  const handlePageChange = (newPage) => {
+    dispatch(getUnifiedOrders(newPage));
+  };
 
   return (
     <div className={s.container}>
-      <h1 className={s.title}>Pedidos Unificados</h1>
-      <TableUnified />
+      <TableUnified
+        unifiedOrders={unifiedOrders}
+        total={total}
+        totalPages={totalPages}
+        page={page}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 };

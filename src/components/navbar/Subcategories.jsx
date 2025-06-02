d24996d52741313d60f4a64afbe56653fdff6921
Franco <mansilla.franco.1@gmail.com>
2025-06-02 15:33:28 -0300

    cambios

--- src/components/navbar/Subcategories.jsx
+++ src/components/navbar/Subcategories.jsx
@@ -1,13 +1,33 @@
+import { useNavigate } from 'react-router-dom';
 import s from './Subcategories.module.css';
 
 const Subcategories = ({ categories }) => {
+  const navigate = useNavigate();
+
+  const handleClick = (category) => {
+    const parentId = category.parent.id;
+    const subcategoryId = category.id;
+    // navigate(`/tienda/${parentId}/${subcategoryId}`, {
+    //   state: { category }
+    // });
+    navigate(
+      `/tienda?type=product&category=${parentId}&subcategory=${subcategoryId}&gender=&sortBy=&searchTerm=`
+    );
+  };
+
   return(
     <div className={s.container}>
       <h5 className={s.title}>{categories[0].parent.name}</h5>
       <hr className={s.divider} />
       <ul className={s.ul}>
         {categories.map(category => (
-          <li key={category.id} className={s.li}>{category.name}</li>
+          <li 
+            key={category.id} 
+            className={s.li}
+            onClick={() => handleClick(category)}
+          >
+            {category.name}
+          </li>
         ))}
       </ul>
     </div>

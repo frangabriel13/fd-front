cb3ed7c5dbb2105b3515c51296f6319383e259cc
Franco <mansilla.franco.1@gmail.com>
2025-06-06 20:13:55 -0300

    cambios

--- src/components/shopComponents/ModalCategory.jsx
+++ src/components/shopComponents/ModalCategory.jsx
@@ -1,9 +1,33 @@
 import s from './ModalCategory.module.css';
 
-const ModalCategory = () => {
+const ModalCategory = ({ categories, selected, onSelect, onClose }) => {
   return (
-    <div>jadf</div>
-  )
+    <div className={s.modal}>
+      <div className={s.modalContent}>
+        <div className={s.container}>
+          <div className={s.divTitle}>
+            <h3 className={s.title}>Selecciona una categoría</h3>
+          </div>
+          <div className={s.divCategories}>
+            {categories.map(cat => (
+              <button
+                key={cat.id}
+                className={`${s.categoryBtn} ${selected === cat.id ? s.selected : ''}`}
+                onClick={() => onSelect(cat.id)}
+              >
+                {cat.name}
+              </button>
+            ))}
+          </div>
+          <div className={s.divBtns}>
+          <button className={s.btnCancel} onClick={onClose}>
+            Cerrar
+          </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 

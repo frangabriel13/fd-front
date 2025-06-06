32d44aa87dcd8863d8ef9ab33c260d59ebfa3b1d
Franco <mansilla.franco.1@gmail.com>
2025-06-06 10:37:09 -0300

    cambios

---
+++ src/components/shopComponents/Shopping.jsx
@@ -0,0 +1,12 @@
+import s from './Shopping.module.css';
+
+const Shopping = () => {
+  return (
+    <div className={s.categorySelection}>
+      <h3>Shopping</h3>
+    </div>
+  );
+};
+
+
+export default Shopping;
---
+++ src/components/shopComponents/Stepper.jsx
@@ -0,0 +1,12 @@
+import s from './Stepper.module.css';
+
+const Stepper = () => {
+  return (
+    <div className={s.categorySelection}>
+      <h3>Stepper</h3>
+    </div>
+  );
+};
+
+
+export default Stepper;

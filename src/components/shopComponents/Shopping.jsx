025013d27c3a68f67929412c9aff3a86b6ffabda
Franco <mansilla.franco.1@gmail.com>
2025-06-08 14:41:15 -0300

    cambios

--- src/components/shopComponents/Shopping.jsx
+++ src/components/shopComponents/Shopping.jsx
@@ -1,17 +1,20 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../../store/actions/productActions';
 import s from './Shopping.module.css';
 
 const Shopping = () => {
   const dispatch = useDispatch();
+  const { products } = useSelector(state => state.product);
   const { category, gender, subcategory } = useParams();
 
   useEffect(() => {
     const filters = { category, gender, subcategory };
     dispatch(getProducts(1, 24, filters, ''));
   }, [dispatch, category, gender, subcategory]);
+
+  console.log('Products:', products);
 
   return (
     <div className={s.categorySelection}>

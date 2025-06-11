808f502f2d907e9fbd1b4d218d19fde24a640ad4
Franco <mansilla.franco.1@gmail.com>
2025-06-11 16:32:00 -0300

    cambios

---
+++ src/pages/packs/Packs.jsx
@@ -0,0 +1,14 @@
+import s from './Packs.module.css';
+
+const Packs = () => {
+  return (
+    <div className={s.container}>
+      <div className={s.divHeader}>
+        <h2 className={s.title}>Packs/Combos</h2>
+      </div>
+    </div>
+  );
+};
+
+
+export default Packs;

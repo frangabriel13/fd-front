3645db1d13294918cf68cd33feee4c3574067502
Franco <mansilla.franco.1@gmail.com>
2025-06-12 18:08:17 -0300

    cambios

--- src/components/shopping/GenderFilter.jsx
+++ src/components/shopping/GenderFilter.jsx
@@ -1,8 +1,19 @@
 import s from './GenderFilter.module.css';
+import { genders } from '../../utils/hardcodeo';
 
-const GenderFilter = () => {
+const GenderFilter = ({ selectedGender, onSelect }) => {
   return (
-    <div className={s.container}>allala</div>
+    <div className={s.container}>
+      {genders.map(gender => (
+        <button
+          key={gender.id}
+          onClick={() => onSelect(gender.id)}
+          className={`${s.button} ${selectedGender === gender.id ? s.selected : ''}`}
+        >
+          {gender.name}
+        </button>
+      ))}
+    </div>
   )
 };
 

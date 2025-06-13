6de02f2f6f9d285b4ebdf74e9fd733da18caee9c
Franco <mansilla.franco.1@gmail.com>
2025-06-13 13:11:44 -0300

    cambios

---
+++ src/components/shopping/CategoryFilter.jsx
@@ -0,0 +1,12 @@
+import s from './CategoryFilter.module.css';
+
+const CategoryFilter = () => {
+  return (
+    <div className={s.container}>
+      sigsilghsuodf
+    </div>
+  );
+};
+
+
+export default CategoryFilter;
--- src/pages/shopping/Shopping.jsx
+++ src/pages/shopping/Shopping.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import GenderFilter from '../../components/shopping/GenderFilter';
+import CategoryFilter from '../../components/shopping/CategoryFilter';
 import s from './Shopping.module.css';
 
 const Shopping = () => {

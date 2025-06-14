438448481f284da6541470d7d4807bf3fc98760d
Franco <mansilla.franco.1@gmail.com>
2025-06-14 16:56:57 -0300

    cambios

--- src/components/shopping/Catalog.jsx
+++ src/components/shopping/Catalog.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import ProductCard from '../productStore/ProductCard';
@@ -11,10 +11,11 @@ const Catalog = ({ genderId, categoryId }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { products, loading, error, currentPage, totalProducts } = useSelector(state => state.product);
+  const [sortBy, setSortBy] = useState('newest');
 
   useEffect(() => {
-    dispatch(getProducts(currentPage, 24, genderId, categoryId));
-  }, [dispatch, genderId, categoryId, currentPage]);
+    dispatch(getProducts(currentPage, 24, genderId, categoryId, sortBy));
+  }, [dispatch, genderId, categoryId, currentPage, sortBy]);
 
   const handleBack = () => {
     navigate('/tienda');
@@ -22,6 +23,10 @@ const Catalog = ({ genderId, categoryId }) => {
 
   const handlePageChange = (page) => {
     dispatch(getProducts(page, 24, genderId, categoryId));
+  };
+
+  const handleSortChange = (e) => {
+    setSortBy(e.target.value);
   };
 
   return (
@@ -35,25 +40,34 @@ const Catalog = ({ genderId, categoryId }) => {
           Volver
         </button>
       </div>
-      <div className={s.productList}>
-        {loading ? (
-          <p>Cargando productos...</p>
-        ) : error ? (
-          <p>Error al cargar productos: {error}</p>
-        ) : products.length === 0 ? (
-          <p>No hay productos disponibles.</p>
-        ) : (
-          products.map(product => (
-            <ProductCard 
-              key={product.id}
-              id={product.id}
-              name={product.name}
-              image={product.mainImage}
-              price={product.price}
-              logo={product.logo}
-            />
-          ))
-        )}
+      <div className={s.divProducts}>
+        <div className={s.divSelect}>
+          <select className={s.select} value={sortBy} onChange={handleSortChange}>
+            <option value="newest">Más Nuevos</option>
+            <option value="lowestPrice">Menor precio</option>
+            <option value="highestPrice">Mayor precio</option>
+          </select>
+        </div>
+        <div className={s.productList}>
+          {loading ? (
+            <p>Cargando productos...</p>
+          ) : error ? (
+            <p>Error al cargar productos: {error}</p>
+          ) : products.length === 0 ? (
+            <p>No hay productos disponibles.</p>
+          ) : (
+            products.map(product => (
+              <ProductCard 
+                key={product.id}
+                id={product.id}
+                name={product.name}
+                image={product.mainImage}
+                price={product.price}
+                logo={product.logo}
+              />
+            ))
+          )}
+        </div>
       </div>
       <Pagination 
         totalProducts={totalProducts} 

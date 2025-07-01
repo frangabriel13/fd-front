de523437438e4f68c09f2be1548416e8045cbb56
Franco <mansilla.franco.1@gmail.com>
2025-07-01 17:51:57 -0300

    cambios

--- src/components/sidebarAccount/SidebarMobile.jsx
+++ src/components/sidebarAccount/SidebarMobile.jsx
@@ -22,49 +22,82 @@ const SidebarMobile = ({ role }) => {
     <div className={s.container}>
       {role === "manufacturer" && (
         <>
-          <Link to="/mi-cuenta/subir-producto" className={s.iconLink}>
-            <BsUpload size={24} />
-          </Link>
-          <Link to="/mi-cuenta/publicaciones" className={s.iconLink}>
-            <BsTag size={24} />
-          </Link>
-          <Link to="/mi-cuenta/packs" className={s.iconLink}>
-            <BsBoxSeam size={24} />
-          </Link>
-          <Link to="/mi-cuenta/ordenes" className={s.iconLink}>
-            <BsClipboardCheck size={24} />
-          </Link>
+          <div className={s.tooltipContainer}>
+            <Link to="/mi-cuenta/subir-producto" className={s.iconLink}>
+              <BsUpload size={24} />
+            </Link>
+            <span className={s.tooltip}>Subir</span>
+          </div>
+          <div className={s.tooltipContainer}>
+            <Link to="/mi-cuenta/publicaciones" className={s.iconLink}>
+              <BsTag size={24} />
+            </Link>
+            <span className={s.tooltip}>Productos</span>
+          </div>
+          <div className={s.tooltipContainer}>
+            <Link to="/mi-cuenta/packs" className={s.iconLink}>
+              <BsBoxSeam size={24} />
+            </Link>
+            <span className={s.tooltip}>Combos</span>
+          </div>
+          <div className={s.tooltipContainer}>
+            <Link to="/mi-cuenta/ordenes" className={s.iconLink}>
+              <BsClipboardCheck size={24} />
+            </Link>
+            <span className={s.tooltip}>Órdenes</span>
+          </div>
         </>
       )}
       {role === "wholesaler" && (
         <>
-          <Link to="/mi-cuenta/favoritos" className={s.iconLink}>
-            <BsHeart size={24} />
-          </Link>
-          <Link to="/mi-cuenta/compras" className={s.iconLink} >
-            <BsCart size={24} />
-          </Link>
-          <Link to="/mi-cuenta/seguidos" className={s.iconLink}>
-            <BsPersonCheck size={24} />
-          </Link>
+          <div className={s.tooltipContainer}>
+            <Link to="/mi-cuenta/favoritos" className={s.iconLink}>
+              <BsHeart size={24} />
+            </Link>
+            <span className={s.tooltip}>Favoritos</span>
+          </div>
+          <div className={s.tooltipContainer}>
+            <Link to="/mi-cuenta/compras" className={s.iconLink} >
+              <BsCart size={24} />
+            </Link>
+            <span className={s.tooltip}>Compras</span>
+          </div>
+          <div className={s.tooltipContainer}>
+            <Link to="/mi-cuenta/seguidos" className={s.iconLink}>
+              <BsPersonCheck size={24} />
+            </Link>
+            <span className={s.tooltip}>Seguidos</span>
+          </div>
         </>
       )}
       {role === "admin" && (
         <>
-          <Link to="/mi-cuenta/usuarios" className={s.iconLink}>
-            <LiaUsersCogSolid size={24} />
-          </Link>
-          <Link to="/mi-cuenta/ordenes-unificadas" className={s.iconLink}>
-            <BsBoxSeam size={24} />
-          </Link>
+          <div className={s.tooltipContainer}>
+            <Link to="/mi-cuenta/usuarios" className={s.iconLink}>
+              <LiaUsersCogSolid size={24} />
+            </Link>
+            <span className={s.tooltip}>Usuarios</span>
+          </div>
+          <div className={s.tooltipContainer}>
+            <Link to="/mi-cuenta/ordenes-unificadas" className={s.iconLink}>
+              <BsBoxSeam size={24} />
+            </Link>
+            <span className={s.tooltip}>Órdenes</span>
+          </div>
         </>
       )}
-      <Link to="/mi-cuenta/" className={s.iconLink}>
-        <PiUserCircle size={24} />
-      </Link>
-      <button onClick={handleLogout} className={s.iconButton}>
-        <CiLogout size={24} />
-      </button>
+      <div className={s.tooltipContainer}>
+        <Link to="/mi-cuenta/" className={s.iconLink}>
+          <PiUserCircle size={24} />
+        </Link>
+        <span className={s.tooltip}>Perfil</span>
+      </div>
+      <div className={s.tooltipContainer}>
+        <button onClick={handleLogout} className={s.iconButton}>
+          <CiLogout size={24} />
+        </button>
+        <span className={s.tooltip}>Salir</span>
+      </div>
     </div>
   )
 };
